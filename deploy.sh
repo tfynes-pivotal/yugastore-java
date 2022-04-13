3e5c129e0b0622474be2f7b751c34cf5b50f4914
Mark Fynes <tfynes@vmware.com>
2022-04-13 11:29:42 -0400

    checkout workload yaml yugabyte ip

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+kubectl apply -f https://raw.githubusercontent.com/tfynes-pivotal/yugastore-java/master/cart-microservice/workload.yaml
+kubectl apply -f https://raw.githubusercontent.com/tfynes-pivotal/yugastore-java/master/checkout-microservice/workload.yaml
+kubectl apply -f https://raw.githubusercontent.com/tfynes-pivotal/yugastore-java/master/eureka-server-local/workload.yaml
+kubectl apply -f https://raw.githubusercontent.com/tfynes-pivotal/yugastore-java/master/login-microservice/workload.yaml
+kubectl apply -f https://raw.githubusercontent.com/tfynes-pivotal/yugastore-java/master/products-microservice/workload.yaml
+
